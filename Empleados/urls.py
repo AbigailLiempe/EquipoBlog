cc63fe5dac2ddf90663701e4faff6773eaff39e5
AbigailLiempe <97003707+AbigailLiempe@users.noreply.github.com>
2022-05-30 12:30:27 -0300

    Editar path
    Editar path

--- Empleados/urls.py
+++ Empleados/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path(r'editar_Equipo/<pk>', views.EquipoUpdate.as_view(), name='Edit'),
     path(r'borrar_Equipo/<pk>', views.EquipoDelete.as_view(), name='Delete'),
 
-    path('login', views.login_request, name = 'login'),
+    path('login', views.login_request, name = 'Login'),
     path('logout', LogoutView.as_view(template_name='Empleado/logout.html'), name='Logout'),
     path('register', views.register, name = 'Register'),
     path("editarUsuario", views.editarUsuario, name="EditarUsuario"),
